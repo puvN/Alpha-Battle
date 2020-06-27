9a2f9e7c0f136e3ee7009d00a4a79bbbbc95ec53
EugeniyLukin <eugeniylukin@gmail.com>
2020-06-27 16:56:39 +0300

    added distance in service three

--- service-three/src/main/java/com/puvn/servicethree/controller/SystemController.java
+++ service-three/src/main/java/com/puvn/servicethree/controller/SystemController.java
@@ -1,6 +1,8 @@
 package com.puvn.servicethree.controller;
 
+import com.puvn.servicethree.entity.Branch;
 import com.puvn.servicethree.exception.ExceptionDTO;
+import com.puvn.servicethree.lat.Distancer;
 import com.puvn.servicethree.repository.BranchRepository;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +12,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Системный контроллер приложения.
@@ -39,6 +45,26 @@ public class SystemController {
 		} else {
 			return ResponseEntity.of(entity);
 		}
+	}
+
+	@ApiOperation(value = "Возвращает Branch по его id", notes = "Возвращает Branch")
+	@GetMapping(value = "/branches")
+	public @ResponseBody
+	ResponseEntity<?> getLon(@RequestParam(name = "lat") Double lat, @RequestParam(name = "lon") Double lon) {
+		var branches = this.branchRepository.findAll();
+		Branch result = null;
+		Map<Long, Double> branchesByLat = new HashMap<>();
+		Distancer distancer = new Distancer();
+		branches.forEach(branch ->
+				branchesByLat.put(branch.getId(), distancer.distance(branch.getLat(), lat, branch.getLon(), lon)));
+		Map.Entry<Long, Double> minEntry = null;
+		for (Map.Entry<Long, Double> entry : branchesByLat.entrySet()) {
+			if (minEntry == null || entry.getValue().compareTo(minEntry.getValue()) < 0) {
+				minEntry = entry;
+			}
+		}
+		var entity = this.branchRepository.findById(minEntry.getKey());
+		return ResponseEntity.of(entity);
 	}
 
 }
